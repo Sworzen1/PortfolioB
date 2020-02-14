5d780d3e53374826f46c96fecaa8c41ab35c45d8
Bartlomiej Sworzen <b.sworzen@gmail.com>
2020-02-15 00:09:30 +0100

    Create box technologie

--- src/Block3.js
+++ src/Block3.js
@@ -1,19 +1,69 @@
-import React from "react"
-import {makeStyles} from "@material-ui/core/styles"
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-     container: {
-        background: 'linear-gradient(180deg, #0d1422 10%, #50021c 100%)',
-        height:"100vh"
-     },
-})
+  container: {
+    background: "linear-gradient(180deg, #0d1422 10%, #50021c 100%)",
+    height: "100vh"
+  },
+
+  icon: {
+    position: "absolute",
+    display: "flex",
+    width: "150px",
+    height: "150px",
+    backgroundColor: "yellow",
+    zIndex: 1,
+    top: 20,
+    left: 20
+  },
+  frame: {
+    position: "absolute",
+    display: "flex",
+    width: "150px",
+    height: "150px",
+    border: "3px solid black",
+    transition: "0.5s",
+  },
+  frameHover: {
+    position: "absolute",
+    display: "flex",
+    width: "150px",
+    height: "150px",
+    border: "3px solid black",
+    transform: "scale(0.95,0.95)",
+    transition: "0.5s",
+  },
+  box: {
+    backgroundColor: "green",
+    position: "relative",
+    width: "165px",
+    height: "165px"
+  }
+});
 
 const Block3 = () => {
-    const classes = useStyles()
+  const classes = useStyles();
+  const [hover, setHover] = useState(true);
 
-    return (
-        <div className={classes.container} id="section3"> </div>
-    )
-}
+  return (
+    <div className={classes.container} id="section3">
+      <div className={classes.box}>
+        <div
+          className={classes.icon}
+          onMouseLeave={() => setHover(true)}
+          onMouseEnter={() => setHover(false)}
+        >
+          Hello Baby
+        </div>
+        <span
+          onMouseLeave={() => setHover(true)}
+          onMouseEnter={() => setHover(false)}
+          className={hover ? classes.frame : classes.frameHover}
+        ></span>
+      </div>
+    </div>
+  );
+};
 
-export default Block3
+export default Block3;
