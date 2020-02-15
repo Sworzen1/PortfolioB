f68f78541717175bb3dd8a806f046e11f3e69669
Bartlomiej Sworzen <b.sworzen@gmail.com>
2020-02-15 01:27:01 +0100

    Add icons, blocks with technologies

--- src/Block3.js
+++ src/Block3.js
@@ -4,42 +4,68 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles({
   container: {
     background: "linear-gradient(180deg, #0d1422 10%, #50021c 100%)",
-    height: "100vh"
+    height: "100vh",
+    gridTemplateColumns: "350px 350px 350px 150px",
+    display: "grid"
+  },
+  box: {
+    position: "relative",
+    width: "181px",
+    height: "181px",
+ marginTop:150,
+ marginLeft:90
   },
 
-  icon: {
+  iconBox: {
     position: "absolute",
     display: "flex",
     width: "150px",
     height: "150px",
-    backgroundColor: "yellow",
+    backgroundColor: "#e6e6e6",
+    border:"1px solid black",
     zIndex: 1,
-    top: 20,
-    left: 20
+    top: 30,
+    left: 30,
+    cursor:"pointer"
+    
   },
   frame: {
     position: "absolute",
     display: "flex",
-    width: "150px",
-    height: "150px",
+    width: "145px",
+    height: "145px",
     border: "3px solid black",
     transition: "0.5s",
   },
   frameHover: {
     position: "absolute",
     display: "flex",
-    width: "150px",
-    height: "150px",
-    border: "3px solid black",
-    transform: "scale(0.95,0.95)",
+    width: "145px",
+    height: "145px",
+    border: "3px solid yellow",
+    transform: "scale(0.89,0.89)",
     transition: "0.5s",
   },
-  box: {
-    backgroundColor: "green",
+  icona:{
+    color:"black",
+    fontSize:"80px",
+    top: "-40%",
+    left: "50%",
     position: "relative",
-    width: "165px",
-    height: "165px"
+    transform:"translate(-50%,0%)",
+    transition: "0.5s",
+  },
+  iconaHover: {
+    color:"yellow",
+    fontSize:"80px",
+    top: "-40%",
+    left: "50%",
+    position: "relative",
+    transform:"translate(-50%,0%)",
+    transition: "0.5s",
   }
+
+
 });
 
 const Block3 = () => {
@@ -48,13 +74,130 @@ const Block3 = () => {
 
   return (
     <div className={classes.container} id="section3">
+
+        {/* HTML */}
+
       <div className={classes.box}>
         <div
-          className={classes.icon}
+          className={classes.iconBox}
           onMouseLeave={() => setHover(true)}
           onMouseEnter={() => setHover(false)}
         >
-          Hello Baby
+        <p className={hover ? classes.icona : classes.iconaHover}>
+          <i class="fab fa-html5" ></i></p>
+        </div>
+        <span
+          onMouseLeave={() => setHover(true)}
+          onMouseEnter={() => setHover(false)}
+          className={hover ? classes.frame : classes.frameHover}
+        ></span>
+      </div>
+
+        {/* CSS */}
+
+      <div className={classes.box}>
+        <div
+          className={classes.iconBox}
+          onMouseLeave={() => setHover(true)}
+          onMouseEnter={() => setHover(false)}
+        >
+            <p className={hover ? classes.icona : classes.iconaHover}>
+          <i class="fab fa-css3-alt" ></i>
+          </p>
+        </div>
+        <span
+          onMouseLeave={() => setHover(true)}
+          onMouseEnter={() => setHover(false)}
+          className={hover ? classes.frame : classes.frameHover}
+        ></span>
+      </div>
+
+        {/* JS */}
+
+
+      <div className={classes.box}>
+        <div
+          className={classes.iconBox}
+          onMouseLeave={() => setHover(true)}
+          onMouseEnter={() => setHover(false)}
+        >
+            <p className={hover ? classes.icona : classes.iconaHover}>
+          <i class="fab fa-js-square" ></i>
+          </p>
+        </div>
+        <span
+          onMouseLeave={() => setHover(true)}
+          onMouseEnter={() => setHover(false)}
+          className={hover ? classes.frame : classes.frameHover}
+        ></span>
+      </div>
+
+        {/* REACT */}
+
+      <div className={classes.box}>
+        <div
+          className={classes.iconBox}
+          onMouseLeave={() => setHover(true)}
+          onMouseEnter={() => setHover(false)}
+        >
+            <p className={hover ? classes.icona : classes.iconaHover}>
+          <i class="fab fa-react" ></i>
+          </p>
+        </div>
+        <span
+          onMouseLeave={() => setHover(true)}
+          onMouseEnter={() => setHover(false)}
+          className={hover ? classes.frame : classes.frameHover}
+        ></span>
+      </div>
+
+        {/* JAVA */}
+
+      <div className={classes.box}>
+        <div
+          className={classes.iconBox}
+          onMouseLeave={() => setHover(true)}
+          onMouseEnter={() => setHover(false)}
+        >
+            <p className={hover ? classes.icona : classes.iconaHover}>
+          <i class="fab fa-java" ></i>
+          </p>
+        </div>
+        <span
+          onMouseLeave={() => setHover(true)}
+          onMouseEnter={() => setHover(false)}
+          className={hover ? classes.frame : classes.frameHover}
+        ></span>
+      </div>
+
+        {/* SQL */}
+
+        <div className={classes.box}>
+        <div
+          className={classes.iconBox}
+          onMouseLeave={() => setHover(true)}
+          onMouseEnter={() => setHover(false)}
+        >
+            <p className={hover ? classes.icona : classes.iconaHover}>
+          <i class="fas fa-database" ></i>
+          </p>
+        </div>
+        <span
+          onMouseLeave={() => setHover(true)}
+          onMouseEnter={() => setHover(false)}
+          className={hover ? classes.frame : classes.frameHover}
+        ></span>
+      </div>
+
+      {/* PHOTOSHOP */}
+
+      <div className={classes.box}>
+        <div
+          className={classes.iconBox}
+          onMouseLeave={() => setHover(true)}
+          onMouseEnter={() => setHover(false)}
+        >
+          PS
         </div>
         <span
           onMouseLeave={() => setHover(true)}
