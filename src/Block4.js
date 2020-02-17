473c9c4b649d6ecdd0a3a31e3c5af8040e2b8a9c
Bartlomiej Sworzen <b.sworzen@gmail.com>
2020-02-18 00:35:17 +0100

    Add example project in Projects section

--- src/Block4.js
+++ src/Block4.js
@@ -1,5 +1,6 @@
-import React from "react"
+import React, {useState} from "react"
 import {makeStyles} from "@material-ui/core/styles"
+import xd from "./xd.jpg"
 
 const useStyles = makeStyles({
     container:{
@@ -14,19 +15,67 @@ const useStyles = makeStyles({
         padding:"30px",
         fontWeight: 300,
         marginTop:90
-    }
+    },
+    box:{
+        backgroundColor:"black",
+        height:290,
+        width:290,
+        position:"relative",
+        left:200,
+        top:100,
+        transition:"1s",
+    },
+    box1:{
+        backgroundColor:"orange",
+        height:290,
+        width:290,
+        position:"relative",
+        left:200,
+        top:100,
+        transition:"1s",
+        cursor:"pointer"
+    },
+    img:{
+        position:"relative",
+        height:290,
+        width:290,
+        filter:"blur(4px) grayscale(1)",
+        backgroundImage:`url(${xd})`,
+        backgroundSize:"cover",
+        transition:"1s"
+    },
+        img1:{
+        position:"relative",
+        height:290,
+        width:290,
+        filter:"blur(0px) grayscale(0)",
+        backgroundImage:`url(${xd})`,
+        backgroundSize:"cover",
+        transition:"1.5s"
+    },
+    
 })
 
 const Block4 = () => {
     const classes = useStyles()
+    const [hover, setHover] = useState(true)
 
     return (
         <div className={classes.container} id="section4"> 
         <div>
             <h1 className={classes.title}>Projects</h1>
         </div>
+        <div className={classes.projects}>
+
+            <div className={hover ? classes.box : classes.box1} onMouseLeave={() => setHover(true)}
+          onMouseEnter={() => setHover(false)}>
+                <div className={hover ? classes.img : classes.img1}/>
+                    </div>
+                </div>
 
         </div>
+
+
     )
 }
 export default Block4 
