5986fe6233bf37d9baf0a7eadd457bb789086348
Bartlomiej Sworzen <b.sworzen@gmail.com>
2020-02-18 14:06:46 +0100

    Import component contact to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import Nav from "./Nav"
 import Block1 from "./Block1"
 import Info from "./Info"
-import Block2 from './Block2';
-import Block3 from "./Block3";
+import Block2 from './Block2'
+import Block3 from "./Block3"
 import Block4 from "./Block4"
+import Block5 from "./Block5"
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
       <Block2 />
       <Block3 />
       <Block4 />
-
+      <Block5 />
     </div>
   );
 }
